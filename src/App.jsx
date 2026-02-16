bd46635d972cabb7af11687ccdd4c01aaa15d9c2
louie <avenido.earllouieo@gmail.com>
2026-02-16 19:00:36 +0800

    Initial project setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./components/Header";
+import AdminDashboard from "./pages/AdminDashboard";
+import "./styles/dashboard.css";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <AdminDashboard />
+    </div>
+  );
+}
+
+export default App;
